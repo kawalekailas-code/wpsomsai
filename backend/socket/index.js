e7794605bf03ae4f1aedb7466e280491c42aecec
kawalekailas-code <kawale.kailas@gmail.com>
2026-04-16 16:36:07 +0530

    Update index.js

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -1,12 +1,32 @@
 import { Server } from "socket.io";
 
 export const initSocket = (server) => {
-  const io = new Server(server, { cors: { origin: "*" } });
+  const io = new Server(server, {
+    cors: { origin: "*" }
+  });
 
   io.on("connection", (socket) => {
+
+    // 🟢 JOIN ROOM
     socket.on("join", (phone) => {
       socket.join(phone);
     });
+
+    // 💬 TYPING START
+    socket.on("typing", (phone) => {
+      socket.to(phone).emit("typing", phone);
+    });
+
+    // 💬 TYPING STOP
+    socket.on("stop_typing", (phone) => {
+      socket.to(phone).emit("stop_typing", phone);
+    });
+
+    // 👀 SEEN EVENT
+    socket.on("seen", (phone) => {
+      socket.to(phone).emit("seen", phone);
+    });
+
   });
 
   return io;
