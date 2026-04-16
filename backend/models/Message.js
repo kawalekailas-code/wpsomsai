3dfc09e6223d748734a5a92ad6d1aa28ea828729
kawalekailas-code <kawale.kailas@gmail.com>
2026-04-16 15:25:17 +0530

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
   phone: String,
+
+  // text OR file name
   message: String,
 
   // incoming / outgoing
@@ -12,13 +14,24 @@ const schema = new mongoose.Schema({
     type: String,
     enum: ["sent", "delivered", "seen"],
     default: "sent"
-  }
+  },
+
+  // 📎 media support
+  media: {
+    type: Boolean,
+    default: false
+  },
+
+  // image / pdf / video
+  mimeType: String
 
 }, {
   timestamps: true // ✅ auto createdAt + updatedAt
 });
 
 // ⏳ auto delete after 30 days
-schema.index({ createdAt: 1 }, { expireAfterSeconds: 60 * 60 * 24 * 30 });
+schema.index({ createdAt: 1 }, {
+  expireAfterSeconds: 60 * 60 * 24 * 30
+});
 
 export default mongoose.model("Message", schema);
