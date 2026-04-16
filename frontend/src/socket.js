2e9ee48d9d8a6838695162583f9c9ec407392e16
kawalekailas-code <kawale.kailas@gmail.com>
2026-04-16 16:40:15 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,2 +1,13 @@
 import { io } from "socket.io-client";
-export const socket = io(process.env.REACT_APP_API);
+
+export const socket = io(process.env.REACT_APP_API, {
+  transports: ["websocket"],
+
+  // 🔥 auto reconnect
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+
+  // optional
+  timeout: 20000
+});
